74bc1bb2d5da7976a3254401d2c9810279254b57
powerfulman86 <dba.ahmedmaher@gmail.com>
2021-10-25 09:24:15 +0200

    Menu

--- models/menu.py
+++ models/menu.py
@@ -14,9 +14,13 @@ class Menu(models.Model):
         purchase_menu = self.env.ref('purchase.menu_purchase_root')
         inventory_menu = self.env.ref('stock.menu_stock_root')
         calendar_menu = self.env.ref('calendar.mail_menu_calendar')
+        account_menu = self.env.ref('account.menu_finance')
+        contact_menu = self.env.ref('contacts.res_partner_menu_config')
 
         menus.discard(sale_menu.id)
         menus.discard(purchase_menu.id)
         menus.discard(inventory_menu.id)
         menus.discard(calendar_menu.id)
+        menus.discard(account_menu.id)
+        menus.discard(contact_menu.id)
         return menus
